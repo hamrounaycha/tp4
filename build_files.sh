09832037a5f5d823cfe7352c966c04ef66530f16
pc2015-PV <hamrounaycha@gmail.com>
2023-01-04 15:18:52 +0100

    vercel settings done done

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "Build Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 
 echo "BuildEnd"
